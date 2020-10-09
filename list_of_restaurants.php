2a2ec04b9aebfa86ec18c4f7ce8d9d48b4288f17
fur120205 <63996501+fur120205@users.noreply.github.com>
2020-10-09 10:55:19 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -24,6 +24,40 @@
 $serverName = "tcp:lab2tp049723.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
   ?>
+      <?php
+ $serverName = "lab2tp049723.database.windows.net,1433";
+ $connectionOptions = array(
+ "Database" => "lab2tp049723",
+ "Uid" => "apuadmin",
+"PWD" => "Asdf1234.");
+
+//Establishes the connection
+ $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if (!$conn)
+{
+ die("Error connection: ".sqlsrv_errors());
+ }
+
+ $tsql= "SELECT * FROM [dbo].[restaurant]";
+ $getResults= sqlsrv_query($conn, $tsql);
+
+if ($getResults == FALSE)
+{
+ die(sqlsrv_errors());
+}
+
+ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+ echo "<tr>";
+ echo "<td>". $row['restaurant_id'] . "</td>";
+ echo "<td>". $row['restaurant_name'] ."</td>";
+ echo "<td>". $row['restaurant_address'] . "</td>";
+ echo "<td>". $row['restaurant_phone'] . "</td>";
+ echo "</tr>";
+ }
+ sqlsrv_free_stmt($getResults);
+?>
  </table>
  </center>
  </body>
